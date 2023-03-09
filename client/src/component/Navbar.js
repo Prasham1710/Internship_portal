3d9c71d953d9d011d7174c9a03f3f76edc0edcef
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-03-10 01:49:11 +0530

    navbar

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react'
-
+import { icons } from 'react-icons/lib'
+import {CgProfile} from 'react-icons/cg'
+import {GrLogout} from 'react-icons/gr'
 const Navbar = () => {
   return (
-    <div className='bg-blue-900 h-14 pl-[900px] '>
-      <div className='text-2xl pl-2'>
-        <button className='pl-4'>Home</button>
-        <button className='pl-4'>Application</button>
-        <button className='pl-4'>Resume</button>
-        <button className='pl-4'>LOR</button>
-        <button className='pl-4'></button>
-        <button>Log Out</button>
+    <div className='bg-blue-900 h-14 pl-[1100px] text-white'>
+      <div className='text-xl pl-2 '>
+        <button className='pl-4 hover:underline'>Home</button>
+        <button className='pl-4 hover:underline'>Application</button>
+        <button className='pl-4 hover:underline'>Resume</button>
+        <button className='pl-4 hover:underline'>LOR</button>
+        <button className='pl-4 pt-2 pr-[10px] hover:underline'><CgProfile size={30}/></button>
+        <button className='pl-2  hover:underline bg-white'><GrLogout /></button>
       </div>
       
         

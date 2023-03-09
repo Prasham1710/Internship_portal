a13b6c62a6a117a5b393b60d462973597cf02ea9
Prasham1710 <jainprasham17@gmail.com>
2023-03-10 01:12:36 +0530

    login page

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,38 +1,41 @@
 import React from 'react'
-import { Grid, Button, Typography,makestyles,Paper} from '@material-ui/core';
+        import {FcGoogle} from 'react-icons/fc'
+        import {AiFillApple, AiFillFacebook} from 'react-icons/ai'
+        import{Link} from 'react-router-dom'
 
-const Login = () => {
-  return (
-     <Paper elevation={3}>      
-     <Grid container direction="column" spacing={4} alignItems="center">
-        <Grid item>
-          <Typography variant="h3" component="h2">
-            Login
-          </Typography>
-        </Grid>
-        <Grid item>
-           <div className=' flex flex-col p-4  '>
+        const Login = () => {
+          return (
+          <div className='relative w-full h-screen  bg-blue-300 '><div className='hidden sm:block'></div>
+                
+            
+
+            <div className='flex justify-center items-center h-full'>
+                <div className='max-w-[400px] w-full mx-auto border rounded-xl bg-white p-4'>
+                        <h2 className='text-3xl font-bold text-center py-5 text-violet-600'>SIGNAL</h2> 
+                        <div className='flex justify-between py-8'>
+                        <button  className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center rounded-xl  '><a href='https://app.aafp.org/sf/services/auth/sso/Apple_SSO?startURL='><AiFillApple /> Apple</a></button>
+                        <button  className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center rounded-xl  '><a href='https://www.facebook.com/login/'><AiFillFacebook />Facebook</a></button>
+                        <button className='border shadow-lg hover:shadow-xl px-6 py-2 relative rounded-xl flex items-center'><a href='https://accounts.google.com/signin'><FcGoogle />Google </a> </button>
+                    </div>
+                  <div className=' flex flex-col p-4  '>
                       <label >Username_</label>
                     <input className=' w-80 border relative  rounded-xl p-1 bg-gray-100' type="text" /></div>
-        </Grid>
-        <Grid item>
-             <div className=' flex flex-col  p-4'> 
+                  <div className=' flex flex-col  p-4'> 
                     <label>Password</label>
-                  <input className=' w-80 border relative  p-1 bg-gray-100' type="password" />
+                  <input className=' w-80 border relative rounded-xl p-1 bg-gray-100' type="password" />
                   </div>
-        </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {}}
-          >
-            Login
-          </Button>
-        </Grid>
-      </Grid>
-    </Paper>
-  )
-}
+                    <button className='border w-[30%] rounded-xl relative my-5 py-1 bg-orange-300 
+                    hover:bg-indigo-500 text-black' >Sign In </button>
+                    <div><p className='flex items-center'>
+                    <input className= 'mr-2 relative'type="checkbox"/> Save password </p>
+                    <label>New User ?</label>
+                    <button className='py-6 relative'><a href ='/create'>Create new account</a></button>
+                  </div>
+                </div>
+            </div>
+            </div> 
+                
+          )
+        }
 
-export default Login
+export default Login;

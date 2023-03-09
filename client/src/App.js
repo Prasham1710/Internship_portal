536ef040a70b9f3cac2cbd202e01d8f102cfda69
Prasham1710 <jainprasham17@gmail.com>
2023-03-09 13:10:41 +0530

    signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Login from './component/Login';
 import Navbar from "./component/Navbar";
+import Signup from "./component/Signup";
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Login />
+      <Signup />
     </div>
   )
 }

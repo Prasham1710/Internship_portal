959473a5c9895adb6b03346cc77d5c75ac085d60
Prasham1710 <jainprasham17@gmail.com>
2023-03-10 09:25:04 +0530

    a

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import Start from './component/Start';
 import Navbar from "./component/Navbar";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './component/Login';
-import Signup from './component/Signupa';
+import Signup from './component/Signup';
+import Logout from './component/Logout.js';
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
       <BrowserRouter><Routes>
       <Route path='/' element={<Start/>} />
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}/>
       </Routes>
       </BrowserRouter>
+      < Logout/>
       
     </div>
   )
--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=''>
-        
+    <div className='h-12 bg-blue-900 font-bold text-xl py-2 pl-[1250px]'>
+        <div className='flex gap-4 '>
+            <button className='hover:underline'>Contact Us</button>
+            <p>|</p>
+            <button className='hover:underline'>Login</button>
+        </div>
     </div>
   )
 }
